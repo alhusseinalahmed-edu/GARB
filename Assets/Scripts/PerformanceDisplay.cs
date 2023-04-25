67d0d5b826ffa3b0e5d6795fe8a7cdb7e19cbc5c
alhusseinalahmed-edu <alhala@edu.umea.se>
2023-04-25 20:08:15 +0200

    Lighting build

--- Assets/Scripts/PerformanceDisplay.cs
+++ Assets/Scripts/PerformanceDisplay.cs
@@ -1,16 +1,19 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
-public class FPSDisplay : MonoBehaviour
+public class PerformanceDisplay : MonoBehaviourPunCallbacks
 {
-    public TMP_Text fpsText;
+    public TMP_Text _text;
     private float deltaTime;
 
     private void Update()
     {
+        int ping = PhotonNetwork.GetPing();
+
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
         float fps = 1.0f / deltaTime;
-        fpsText.text = string.Format("{0:0.} FPS", fps);
+        _text.text = string.Format("{0:0.} FPS", fps) + " " + ping + " ms";
     }
 }
